8e501dd9b5aa0d797ccfc0d6cc1d8b22bae6fb29
Sayanc2000 <sayanc20002@gmail.com>
2023-05-17 14:25:08 +0530

    feat: added response time middleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware
 
 from app import models
 from app.database import engine
-from app.routers import users, auth, blogs
+from app.middleware import add_process_time_header
+from app.routers import users, auth, blogs, tasks
 from app.schemas.basicSchemas import DefaultResponse
 
 app = FastAPI()
@@ -17,9 +19,12 @@ app.add_middleware(
     expose_headers=["*"]
 )
 
+app.add_middleware(BaseHTTPMiddleware, dispatch=add_process_time_header)
+
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(blogs.router)
+app.include_router(tasks.router)
 
 models.Base.metadata.create_all(bind=engine)
 
